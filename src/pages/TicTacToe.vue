c8594d4e767a1ec1167e3ad5852b39ee4fcb94b3
Chiara <chiara.knipprath@iem.thm.de>
2021-12-11 14:30:22 +0100

    first page

---
+++ src/pages/TicTacToe.vue
@@ -0,0 +1,25 @@
+<template>
+    <q-layout view="hHh lpR fFf " class="row">
+    <q-page-container class="game col">
+      <h1>GAME</h1>
+    </q-page-container>
+    <q-page-container class="tree col">
+      <h1>TREE</h1>
+    </q-page-container>
+  </q-layout>
+</template>
+
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TicTacToe',
+  components: { },
+  setup() {
+    
+    return
+  }
+});
+</script>
